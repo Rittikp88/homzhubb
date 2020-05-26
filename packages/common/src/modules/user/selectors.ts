c08c33f0b7b9b3be0973011163a05f267e3ca035
Aditya Warrier <aditya@nineleaps.com>
2020-05-26 13:18:37 +0530

    SocialMediaProvider.ts model issue fixed

--- packages/common/src/modules/user/selectors.ts
+++ packages/common/src/modules/user/selectors.ts
@@ -1,14 +1,13 @@
-import { ObjectMapper } from '@homzhub/common/src/utils/ObjectMapper';
 import { IState } from '@homzhub/common/src/modules/interfaces';
-import { SocialMediaProvider } from '@homzhub/common/src/domain/models/SocialMediaProvider';
+import { ISocialMediaProvider } from '@homzhub/common/src/domain/models/SocialMediaProvider';
 
 const isLoggedIn = (state: IState): boolean => {
   return !!state.user.user;
 };
 
-const getSocialMediaProviders = (state: IState): SocialMediaProvider[] => {
+const getSocialMediaProviders = (state: IState): ISocialMediaProvider[] => {
   const { user } = state;
-  return ObjectMapper.deserializeArray(SocialMediaProvider, user.socialProviders);
+  return user.socialProviders;
 };
 
 export const UserSelector = {
