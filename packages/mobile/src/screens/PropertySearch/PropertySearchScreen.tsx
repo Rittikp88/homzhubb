01e6be9e320c9c6ee40de35ae4a7eb530cd68509
Aditya Warrier <51952197+adityaNL@users.noreply.github.com>
2020-07-16 16:11:23 +0530

    Design of Menu Tray for Property Search Screen

--- packages/mobile/src/screens/PropertySearch/PropertySearchScreen.tsx
+++ packages/mobile/src/screens/PropertySearch/PropertySearchScreen.tsx
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { WithTranslation, withTranslation } from 'react-i18next';
 import { IPropertiesObject } from '@homzhub/common/src/domain/models/Search';
 import { SearchSelector } from '@homzhub/common/src/modules/search/selectors';
 import { IState } from '@homzhub/common/src/modules/interfaces';
 import { theme } from '@homzhub/common/src/styles/theme';
 import { icons } from '@homzhub/common/src/assets/icon';
-import { Label, ToggleButton } from '@homzhub/common/src/components';
+import { Button, ButtonType, FontWeightType, Label, ToggleButton } from '@homzhub/common/src/components';
 import { PropertySearchMap } from '@homzhub/mobile/src/components/organisms/PropertySearchMap';
 import PropertySearchList from '@homzhub/mobile/src/components/organisms/PropertySearchList';
+import { LocaleConstants } from '@homzhub/common/src/services/Localization/constants';
+
+enum OnScreenFilters {
+  TYPE = 'TYPE',
+  PRICE = 'PRICE',
+  ROOMS = 'ROOMS',
+  MORE = 'MORE',
+}
 
 interface IStateProps {
   properties: IPropertiesObject;
@@ -16,13 +25,15 @@ interface IStateProps {
 
 interface IPropertySearchScreenState {
   isMapView: boolean;
+  selectedOnScreenFilter: OnScreenFilters | string;
 }
 
-type Props = IStateProps;
+type Props = WithTranslation & IStateProps;
 
-class PropertySearchScreen extends Component<Props, IPropertySearchScreenState> {
+class PropertySearchScreen extends PureComponent<Props, IPropertySearchScreenState> {
   public state = {
     isMapView: true,
+    selectedOnScreenFilter: '',
   };
 
   public render(): React.ReactNode {
@@ -31,12 +42,69 @@ class PropertySearchScreen extends Component<Props, IPropertySearchScreenState> 
     const containerStyle = isMapView ? styles.container : styles.listContainer;
     return (
       <SafeAreaView style={containerStyle}>
+        {this.renderFilterTray()}
         {/* // TODO: Pass the properties to search map component for getting data */}
         {isMapView ? <PropertySearchMap /> : <PropertySearchList properties={properties} />}
         {this.renderBar()}
       </SafeAreaView>
     );
   }
+
+  private renderFilterTray = (): React.ReactNode => {
+    const { t } = this.props;
+    const { selectedOnScreenFilter } = this.state;
+    const onScreenFilters = [
+      { type: OnScreenFilters.TYPE, label: t('type') },
+      { type: OnScreenFilters.ROOMS, label: t('rooms') },
+      { type: OnScreenFilters.PRICE, label: t('price') },
+      { type: OnScreenFilters.MORE, label: icons.filter },
+    ];
+    return (
+      <View style={styles.menuTrayContainer}>
+        {onScreenFilters.map((item: { type: OnScreenFilters; label: string }, index: number) => {
+          const { type, label } = item;
+          let buttonType: ButtonType = 'secondary';
+          let fontWeight: FontWeightType = 'regular';
+          let iconColor = theme.colors.primaryColor;
+          if (selectedOnScreenFilter === type) {
+            buttonType = 'primary';
+            fontWeight = 'semiBold';
+            iconColor = theme.colors.secondaryColor;
+          }
+
+          const onPress = () => this.setState({ selectedOnScreenFilter: type });
+
+          if (index === 3) {
+            return (
+              <Button
+                key={type}
+                type={buttonType}
+                icon={label}
+                iconColor={iconColor}
+                iconSize={20}
+                onPress={onPress}
+                iconStyle={styles.menuButtonText}
+                containerStyle={styles.filterButtons}
+              />
+            );
+          }
+          return (
+            <Button
+              key={type}
+              title={label}
+              type={buttonType}
+              textType="label"
+              textSize="regular"
+              fontType={fontWeight}
+              onPress={onPress}
+              containerStyle={styles.filterButtons}
+              titleStyle={styles.menuButtonText}
+            />
+          );
+        })}
+      </View>
+    );
+  };
 
   private renderBar = (): React.ReactNode => {
     const { isMapView } = this.state;
@@ -71,7 +139,10 @@ const mapStateToProps = (state: IState): IStateProps => {
   };
 };
 
-export default connect(mapStateToProps, null)(PropertySearchScreen);
+export default connect(
+  mapStateToProps,
+  null
+)(withTranslation(LocaleConstants.namespacesKey.propertySearch)(PropertySearchScreen));
 
 const styles = StyleSheet.create({
   container: {
@@ -82,9 +153,23 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    top: 50,
+    top: theme.viewport.height * 0.14,
     width: theme.viewport.width,
     paddingHorizontal: theme.layout.screenPadding,
+  },
+  filterButtons: {
+    flex: 0,
+  },
+  menuTrayContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: theme.colors.white,
+    paddingHorizontal: theme.layout.screenPadding,
+    paddingVertical: 12,
+  },
+  menuButtonText: {
+    marginVertical: 8,
+    marginHorizontal: 24,
   },
   propertiesFound: {
     backgroundColor: theme.colors.white,
