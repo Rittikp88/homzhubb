59e70e2a34bb2565ec654b659ecd6a61ae88395b
Aditya Warrier <aditya@nineleaps.com>
2020-08-03 11:17:57 +0530

    [mobile] added alert message for whatsapp not installed

--- packages/mobile/src/services/LinkingService.ts
+++ packages/mobile/src/services/LinkingService.ts
@@ -1,4 +1,6 @@
 import { Linking } from 'react-native';
+import { I18nService } from '@homzhub/common/src/services/Localization/i18nextService';
+import { AlertHelper } from '@homzhub/mobile/src/utils/AlertHelper';
 
 class LinkingService {
   public openDialer = async (phoneNumber: string): Promise<void> => {
@@ -12,6 +14,7 @@ class LinkingService {
   public whatsappMessage = async (phoneNumber: string, message: string): Promise<void> => {
     const url = `whatsapp://send?phone=${phoneNumber}&text=${message}`;
     if (!(await this.canOpenURL(url))) {
+      AlertHelper.error({ message: I18nService.t('common:installWhatsapp') });
       return;
     }
     await Linking.openURL(url);
