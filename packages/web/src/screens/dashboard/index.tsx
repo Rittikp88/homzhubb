c073e70bbec5720828139f636027a49cd7606bbd
Bishal Patnaik <bishal.patnaik@nineleaps.com>
2020-11-09 21:17:33 +0530

    removed mock data  for pending properties

--- packages/web/src/screens/dashboard/index.tsx
+++ packages/web/src/screens/dashboard/index.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { ObjectMapper } from '@homzhub/common/src/utils/ObjectMapper';
 import { theme } from '@homzhub/common/src/styles/theme';
-import { PendingPropertiesCard } from '@homzhub/web/src/components';
 import MarketTrendsCarousel from '@homzhub/web/src/screens/dashboard/components/MarketTrendsCarousel';
-import { Asset } from '@homzhub/common/src/domain/models/Asset';
-// TODO: (Bishal) remove this mock file and replace with data from api
-import MockAssetData from '@homzhub/web/src/screens/dashboard/components/MockAssetData.json';
 
 class Dashboard extends React.PureComponent {
   public render(): React.ReactNode {
-    // TODO: (Bishal) - remove this once api is integrated
-    const data = ObjectMapper.deserializeArray(Asset, (MockAssetData as unknown) as Asset[]);
     return (
       <View style={styles.container}>
-        <PendingPropertiesCard data={data} />
         <MarketTrendsCarousel />
       </View>
     );
