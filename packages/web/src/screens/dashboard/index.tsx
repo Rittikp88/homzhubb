8a7be95662b996293ca8e541cfa5dd14c0ae7de9
Bishal Patnaik <bishal.patnaik@nineleaps.com>
2020-11-20 19:13:04 +0530

    property overview component built

--- packages/web/src/screens/dashboard/index.tsx
+++ packages/web/src/screens/dashboard/index.tsx
@@ -2,10 +2,12 @@ import React, { FC } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { theme } from '@homzhub/common/src/styles/theme';
 import MarketTrendsCarousel from '@homzhub/web/src/screens/dashboard/components/MarketTrendsCarousel';
+import PropertyOverview from '@homzhub/web/src/screens/dashboard/components/PropertyOverview';
 
 const Dashboard: FC = () => {
   return (
     <View style={styles.container}>
+      <PropertyOverview />
       <MarketTrendsCarousel />
     </View>
   );
