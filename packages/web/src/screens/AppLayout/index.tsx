b8b488f237eb97d26f2a89b3fd788946acc877d8
Bishal Patnaik <47746943+bishalpatnaik-nineleaps@users.noreply.github.com>
2020-11-24 17:11:04 +0530

    Displayed dashboard pending properties and user subscription layouts with dummy data

--- packages/web/src/screens/AppLayout/index.tsx
+++ packages/web/src/screens/AppLayout/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { useDown } from '@homzhub/common/src/utils/MediaQueryUtils';
+import { FunctionUtils } from '@homzhub/common/src/utils/FunctionUtils';
 import { MainRouter } from '@homzhub/web/src/router/MainRouter';
 import { theme } from '@homzhub/common/src/styles/theme';
 import Footer from '@homzhub/web/src/screens/AppLayout/Footer';
@@ -10,14 +11,11 @@ import { deviceBreakpoint } from '@homzhub/common/src/constants/DeviceBreakpoint
 
 const AppLayout: FC = () => {
   const isTablet = useDown(deviceBreakpoint.TABLET);
-  const sideMenuItemClicked = (ItemId: number): void => {
-    // TODO: side menu item click handler
-  };
   return (
     <View style={styles.container}>
       <Navbar />
       <View style={styles.mainContent}>
-        {!isTablet && <SideMenu onItemClick={sideMenuItemClicked} />}
+        {!isTablet && <SideMenu onItemClick={FunctionUtils.noop} />}
         <MainRouter />
       </View>
       <Footer />
