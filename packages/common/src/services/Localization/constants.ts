d7362d527472b7c15dd30d721eb43ba499075c6d
Aditya Warrier <51952197+adityaNL@users.noreply.github.com>
2020-11-27 19:31:03 +0530

    added langs (#486)

--- packages/common/src/services/Localization/constants.ts
+++ packages/common/src/services/Localization/constants.ts
@@ -2,6 +2,15 @@
 export enum SupportedLanguages {
   English = 'en-US',
   Marathi = 'mr-IN',
+  Hindi = 'hi-IN',
+  Telugu = 'te-IN',
+  Tamil = 'ta-IN',
+  Malayalam = 'ml-IN',
+  Kannada = 'kn-IN',
+  Gujarati = 'gu-IN',
+  Chinese = 'zh-CN',
+  Korean = 'ko',
+  Japanese = 'ja',
 }
 const whitelist = Object.values(SupportedLanguages);
 
@@ -12,6 +21,15 @@ const whitelist = Object.values(SupportedLanguages);
 const resources = {
   [SupportedLanguages.English]: require('@homzhub/common/src/assets/languages/en.json'),
   [SupportedLanguages.Marathi]: require('@homzhub/common/src/assets/languages/mr.json'),
+  [SupportedLanguages.Tamil]: require('@homzhub/common/src/assets/languages/ta.json'),
+  [SupportedLanguages.Telugu]: require('@homzhub/common/src/assets/languages/te.json'),
+  [SupportedLanguages.Gujarati]: require('@homzhub/common/src/assets/languages/gu.json'),
+  [SupportedLanguages.Hindi]: require('@homzhub/common/src/assets/languages/hi.json'),
+  [SupportedLanguages.Malayalam]: require('@homzhub/common/src/assets/languages/ml.json'),
+  [SupportedLanguages.Kannada]: require('@homzhub/common/src/assets/languages/kn.json'),
+  [SupportedLanguages.Korean]: require('@homzhub/common/src/assets/languages/ko.json'),
+  [SupportedLanguages.Japanese]: require('@homzhub/common/src/assets/languages/ja.json'),
+  [SupportedLanguages.Chinese]: require('@homzhub/common/src/assets/languages/zh.json'),
 };
 
 // Add namespaces here
