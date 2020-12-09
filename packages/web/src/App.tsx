16d0c71688f32f1132fac619d220c73f7696699c
Bishal Patnaik <bishal.patnaik@nineleaps.com>
2020-12-09 10:30:20 +0530

    user data persisted on login

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { I18nService } from '@homzhub/common/src/services/Localization/i18nextService';
+import { IUserTokens, StorageKeys, StorageService } from '@homzhub/common/src/services/storage/StorageService';
 import { StoreProviderService } from '@homzhub/common/src/services/StoreProviderService';
-import { configureStore } from '@homzhub/common/src/modules/store';
 import { AppRouter } from '@homzhub/web/src/router/AppRouter';
+import { UserActions } from '@homzhub/common/src/modules/user/actions';
+import { configureStore } from '@homzhub/common/src/modules/store';
 
 interface IState {
   isLocalizationInitialised: boolean;
@@ -18,6 +20,10 @@ export class App extends React.PureComponent<{}, IState> {
   };
 
   public componentDidMount = async (): Promise<void> => {
+    const userData = await StorageService.get<IUserTokens>(StorageKeys.USER);
+    if (userData) {
+      store.dispatch(UserActions.loginSuccess(userData));
+    }
     await I18nService.init();
     this.setState({ isLocalizationInitialised: true });
   };
