3ce1ba13c3eeb9a2704265432e8ce4dc95939f7a
Shikha Rai <48086446+raiShikha@users.noreply.github.com>
2020-12-15 10:39:03 +0530

    HOM-1033: Address Field changes

--- packages/common/src/domain/models/Address.ts
+++ packages/common/src/domain/models/Address.ts
@@ -1,11 +1,17 @@
 import { JsonObject, JsonProperty } from '@homzhub/common/src/utils/ObjectMapper';
+import { Country, ICountry } from '@homzhub/common/src/domain/models/Country';
 import { ILocationDetails, LocationDetails } from '@homzhub/common/src/domain/models/LocationDetails';
 
 export interface IAddress {
   id: number;
+  address: string;
   address_line_1: string;
   address_line_2: string;
   postal_code: string;
+  city_name: string;
+  state_name: string;
+  country_name: string;
+  country: ICountry;
   is_primary: boolean;
   location: ILocationDetails;
 }
@@ -15,23 +21,42 @@ export class Address {
   @JsonProperty('id', Number)
   private _id = 0;
 
-  @JsonProperty('address_line_1', String)
+  @JsonProperty('address', String, true)
+  private _address = '';
+
+  @JsonProperty('address_line_1', String, true)
   private _addressLine1 = '';
 
-  @JsonProperty('address_line_2', String)
+  @JsonProperty('address_line_2', String, true)
   private _addressLine2 = '';
 
   @JsonProperty('postal_code', String)
   private _postalCode = '';
 
+  @JsonProperty('city_name', String, true)
+  private _cityName = '';
+
+  @JsonProperty('state_name', String, true)
+  private _stateName = '';
+
+  @JsonProperty('country_name', String, true)
+  private _countryName = '';
+
   @JsonProperty('is_primary', Boolean)
   private _isPrimary = false;
 
-  @JsonProperty('location', LocationDetails)
-  private _location = new LocationDetails();
+  @JsonProperty('location', LocationDetails, true)
+  private _location = null;
+
+  @JsonProperty('country', Country, true)
+  private _country = new Country();
 
   get id(): number {
     return this._id;
+  }
+
+  get address(): string {
+    return this._address;
   }
 
   get addressLine1(): string {
@@ -50,7 +75,23 @@ export class Address {
     return this._isPrimary;
   }
 
-  get location(): LocationDetails {
+  get location(): LocationDetails | null {
     return this._location;
+  }
+
+  get cityName(): string {
+    return this._cityName;
+  }
+
+  get stateName(): string {
+    return this._stateName;
+  }
+
+  get countryName(): string {
+    return this._countryName;
+  }
+
+  get country(): Country {
+    return this._country;
   }
 }
