7c2c68e52f5a0266aa6125c7585b97d71f044cd9
Bishal Patnaik <bishal.patnaik@nineleaps.com>
2021-01-12 20:47:30 +0530

    location updated in context

--- packages/web/src/components/atoms/GoogleMapView.tsx
+++ packages/web/src/components/atoms/GoogleMapView.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useCallback } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { GoogleMap, Marker } from '@react-google-maps/api';
+import { ILatLng } from '@homzhub/web/src/screens/addProperty/AddPropertyContext';
 
 const containerStyle = {
   width: '100%',
@@ -11,24 +12,20 @@ const centerDefault = {
   lat: 12.930993354465738,
   lng: 77.63309735316916,
 };
-const markerPosition = {
-  lat: 12.930993354465738,
-  lng: 77.63309735316916,
-};
 
 interface IProps {
-  center?: { lat: number; lng: number };
+  center?: ILatLng;
+  updateCenter?: (center: ILatLng) => void;
 }
 
 const GoogleMapView: FC<IProps> = (props: IProps) => {
-  const { center } = props;
-  const [map, setMap] = React.useState<google.maps.Map | null>(null);
+  const { center, updateCenter } = props;
   const onLoad = useCallback((mapInstance: google.maps.Map) => {
-    setMap(mapInstance);
+    // setMap(mapInstance);
   }, []);
 
   const onUnmount = useCallback((mapInstance: google.maps.Map) => {
-    setMap(null);
+    // setMap(null);
   }, []);
   const onLoadMarker = (marker: google.maps.Marker): void => {
     // todos empty
@@ -37,7 +34,11 @@ const GoogleMapView: FC<IProps> = (props: IProps) => {
     // todos empty
   };
   const onDragEnd = (event: google.maps.MapMouseEvent): void => {
-    // todos empty
+    const newCenter = { lat: event.latLng.lat(), lng: event.latLng.lng() } as ILatLng;
+    // setCurrentLatLng(newCenter);
+    if (updateCenter) {
+      updateCenter(newCenter);
+    }
   };
   return (
     <View style={styles.container}>
@@ -51,8 +52,9 @@ const GoogleMapView: FC<IProps> = (props: IProps) => {
         {/* Child components, such as markers, info windows, etc. */}
         <Marker
           onLoad={onLoadMarker}
-          position={center || markerPosition}
+          position={center || centerDefault}
           draggable
+          animation={google.maps.Animation.DROP}
           onDragStart={onDragStart}
           onDragEnd={onDragEnd}
         />
--- packages/web/src/screens/addProperty/components/PropertyDetailsMap.tsx
+++ packages/web/src/screens/addProperty/components/PropertyDetailsMap.tsx
@@ -1,21 +1,53 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { useDown } from '@homzhub/common/src/utils/MediaQueryUtils';
+import { AddPropertyContext, ILatLng } from '@homzhub/web/src/screens/addProperty/AddPropertyContext';
+import { GooglePlacesService } from '@homzhub/common/src/services/GooglePlaces/GooglePlacesService';
 import { theme } from '@homzhub/common/src/styles/theme';
 import GoogleMapView from '@homzhub/web/src/components/atoms/GoogleMapView';
 import PropertyDetailsForm from '@homzhub/web/src/screens/addProperty/components/PropertyDetailsForm';
 import { deviceBreakpoint } from '@homzhub/common/src/constants/DeviceBreakpoints';
-import { AddPropertyContext } from '@homzhub/web/src/screens/addProperty/AddPropertyContext';
+
+const getDataFromPlaceID = (placeID: string, callback: (geocoderResult: google.maps.GeocoderResult) => void): void => {
+  const geocoder = new google.maps.Geocoder();
+  geocoder.geocode({ placeId: placeID }, (results, status) => {
+    if (status === 'OK') {
+      if (results[0]) {
+        callback(results[0]);
+      } else {
+        // window.alert('No results found');
+      }
+    } else {
+      // window.alert('Geocoder failed due to: ' + status);
+    }
+  });
+};
 
 const PropertyDetailsMap: FC = () => {
   const isTablet = useDown(deviceBreakpoint.TABLET);
-  const { hasScriptLoaded, latLng } = useContext(AddPropertyContext);
-  // todo => getDetails from location or place id whatever is available and display data accordingly currently getPlacedetails is not working
-  // GooglePlacesService.getPlaceDetail(selectedPlaceId).then((r) => console.log(`placeid-----------------------${r}`));
-  // GooglePlacesService.getLocationData(latLng).then((r) => console.log(r));
+  const { hasScriptLoaded, latLng, selectedPlaceId, setUpdatedLatLng } = useContext(AddPropertyContext);
+  useEffect(() => {
+    if (hasScriptLoaded) {
+      if (selectedPlaceId.length !== 0) {
+        getDataFromPlaceID(selectedPlaceId, () => {
+          // update location
+          // transform data
+        });
+      } else if (latLng) {
+        GooglePlacesService.getLocationData(latLng).then((r) => {
+          // update location
+          // transform data
+        });
+      }
+    }
+  }, [latLng, hasScriptLoaded, selectedPlaceId]);
+  const handleMapCenterChange = (newCenter: ILatLng): void => {
+    setUpdatedLatLng(newCenter);
+  };
+
   return (
     <View style={[styles.container, isTablet && styles.containerTablet]}>
-      {hasScriptLoaded && <GoogleMapView center={latLng} />}
+      {hasScriptLoaded && <GoogleMapView center={latLng} updateCenter={handleMapCenterChange} />}
       <PropertyDetailsForm />
     </View>
   );
