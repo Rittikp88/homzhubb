d5518a8fe0507d4193e90505823fc419d3bf21da
Shikha Rai <shikharai@nineleaps.com>
2021-01-19 00:00:09 +0530

    minor fix for set user

--- packages/common/src/services/Analytics/AnalyticsService.ts
+++ packages/common/src/services/Analytics/AnalyticsService.ts
@@ -1,7 +1,7 @@
 import { ConfigHelper } from '@homzhub/common/src/utils/ConfigHelper';
 import { StoreProviderService } from '@homzhub/common/src/services/StoreProviderService';
 import { User } from '@homzhub/common/src/domain/models/User';
-import { EventDataType } from '@homzhub/common/src/services/Analytics/interfaces';
+import { AuthenticationType, EventDataType } from '@homzhub/common/src/services/Analytics/interfaces';
 import { EventType } from '@homzhub/common/src/services/Analytics/EventType';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -18,7 +18,7 @@ class AnalyticsService {
 
   public setUser = (user: User): void => {
     this.mixPanelInstance.identify(user.email);
-    const name = user.fullName ?? `${user.firstName} ${user.lastName}`;
+    const name = user.fullName || `${user.firstName} ${user.lastName}`;
     this.mixPanelInstance.people.set({ $email: user.email, $name: name });
   };
 
