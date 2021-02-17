7046396855810fe2d35071497a1e294ecceccad7
Shivam <shivam.pandey@nineleaps.com>
2021-02-17 16:13:03 +0530

    minor changes

--- packages/mobile/src/screens/Asset/More/Messages.tsx
+++ packages/mobile/src/screens/Asset/More/Messages.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { PickerItemProps, StyleSheet, Text } from 'react-native';
-import { WithTranslation, withTranslation } from 'react-i18next';
-import { bindActionCreators, Dispatch } from 'redux';
-import { connect } from 'react-redux';
+import { Text } from 'react-native';
 
 export class Messages extends React.PureComponent {
   public render(): React.ReactNode {
