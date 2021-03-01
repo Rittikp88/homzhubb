f98af276cf218ab742aa1a930daddc50c3ab9c92
Shaguntiwari22 <53553405+Shaguntiwari22@users.noreply.github.com>
2021-03-01 18:37:51 +0530

    HOM-1711: Portfolio filters

---
+++ packages/web/src/screens/portfolio/components/PortfolioFilter.tsx
@@ -0,0 +1,110 @@
+import React, { useRef } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { PopupActions } from 'reactjs-popup/dist/types';
+import { theme } from '@homzhub/common/src/styles/theme';
+import Icon, { icons } from '@homzhub/common/src/assets/icon';
+import { Typography } from '@homzhub/common/src/components/atoms/Typography';
+import Popover from '@homzhub/web/src/components/atoms/Popover';
+import PopupMenuOptions from '@homzhub/web/src/components/molecules/PopupMenuOptions';
+import { AssetFilter } from '@homzhub/common/src/domain/models/AssetFilter';
+
+// TODO: Integration of rest of the filter buttons and optimization: Shagun
+
+interface IPortfolioFilterProps {
+  filterData: AssetFilter[];
+  getStatus: (status: string) => void;
+}
+type IProps = IPortfolioFilterProps;
+const PortfolioFilter: React.FC<IProps> = (props: IProps) => {
+  const { t } = useTranslation();
+
+  const { filterData, getStatus } = props;
+  const popupRef = useRef<PopupActions>(null);
+
+  const popupProps = {
+    position: 'bottom left' as 'bottom left',
+    on: 'click' as 'click',
+    arrow: false,
+    contentStyle: { marginTop: '4px' },
+    closeOnDocumentClick: true,
+    children: undefined,
+  };
+
+  const selectedFilter = (selectedOption: AssetFilter): void => {
+    getStatus(selectedOption.label);
+    closePopup();
+  };
+  const closePopup = (): void => {
+    if (popupRef && popupRef.current) {
+      popupRef.current.close();
+    }
+  };
+
+  return (
+    <View style={styles.filterSection}>
+      <View style={styles.filterContainer}>
+        <Typography size="small" fontWeight="semiBold" style={styles.filterText}>
+          {t('assetPortfolio:selectCountry')}
+        </Typography>
+        <Icon name={icons.downArrow} size={18} style={styles.icon} />
+      </View>
+
+      <View style={styles.filterContainer}>
+        <Typography size="small" fontWeight="semiBold" style={styles.filterText}>
+          {t('assetFinancial:selectProperty')}
+        </Typography>
+        <Icon name={icons.downArrow} size={18} style={styles.icon} />
+      </View>
+
+      <View style={styles.filterContainer}>
+        <Typography size="small" fontWeight="semiBold" style={styles.filterText}>
+          {t('propertySearch:type')}
+        </Typography>
+        <Icon name={icons.downArrow} size={18} style={styles.icon} />
+      </View>
+
+      <View>
+        <Popover
+          forwardedRef={popupRef}
+          content={<PopupMenuOptions options={filterData} onMenuOptionPress={selectedFilter} />}
+          popupProps={popupProps}
+        >
+          <TouchableOpacity activeOpacity={1}>
+            <View style={styles.filterContainer}>
+              <Typography size="small" fontWeight="semiBold" style={styles.filterText}>
+                {t('helpAndSupport:status')}
+              </Typography>
+              <Icon name={icons.downArrow} size={18} style={styles.icon} />
+            </View>
+          </TouchableOpacity>
+        </Popover>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  filterSection: {
+    marginVertical: '2%',
+    flexDirection: 'row',
+  },
+  filterContainer: {
+    backgroundColor: theme.colors.white,
+    justifyContent: 'center',
+    marginEnd: 12,
+    borderRadius: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  filterText: {
+    marginHorizontal: 12,
+    marginVertical: 6,
+    color: theme.colors.primaryColor,
+  },
+  icon: {
+    marginEnd: 12,
+    color: theme.colors.primaryColor,
+  },
+});
+export default PortfolioFilter;
--- packages/web/src/screens/portfolio/index.tsx
+++ packages/web/src/screens/portfolio/index.tsx
@@ -1,12 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { AlertHelper } from '@homzhub/common/src/utils/AlertHelper';
+import { ErrorUtils } from '@homzhub/common/src/utils/ErrorUtils';
+import { PortfolioRepository } from '@homzhub/common/src/domain/repositories/PortfolioRepository';
 import PortfolioHeader from '@homzhub/web/src/screens/portfolio/components/PortfolioHeader';
 import PortfolioCardGroup from '@homzhub/web/src/screens/portfolio/components/PortfolioCardGroup';
+import PortfolioFilter from '@homzhub/web/src/screens/portfolio/components/PortfolioFilter';
+import { AssetFilter } from '@homzhub/common/src/domain/models/AssetFilter';
 
 const Portfolio: FC = () => {
+  const [statusfilters, setStatusFilters] = useState<AssetFilter[]>([]);
+  useEffect(() => {
+    PortfolioRepository.getAssetFilters()
+      .then((response) => {
+        setStatusFilters(response);
+      })
+      .catch((e) => {
+        const error = ErrorUtils.getErrorMessage(e.details);
+        AlertHelper.error({ message: error });
+      });
+  }, []);
+  const getStatus = (status: string): void => {
+    PortfolioRepository.getUserAssetDetails(status)
+      .then((response) => {
+        // TODO :Use the response to display filteredcard :Mohak
+      })
+      .catch((e) => {
+        const error = ErrorUtils.getErrorMessage(e.details);
+        AlertHelper.error({ message: error });
+      });
+  };
   return (
     <View style={styles.container}>
       <PortfolioHeader />
+      <PortfolioFilter filterData={statusfilters} getStatus={getStatus} />
       <PortfolioCardGroup />
     </View>
   );
