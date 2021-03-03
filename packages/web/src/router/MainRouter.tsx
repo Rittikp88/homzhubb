39238d5f29d4a94546cb709924f135b2cf8af266
ashwin712 <ashwin.karthik@nineleaps.com>
2021-03-03 20:51:01 +0530

    Route for Search Property

--- packages/web/src/router/MainRouter.tsx
+++ packages/web/src/router/MainRouter.tsx
@@ -5,13 +5,14 @@ import { RouteNames } from '@homzhub/web/src/router/RouteNames';
 import Dashboard from '@homzhub/web/src/screens/dashboard';
 import AddPropertyListing from '@homzhub/web/src/screens/addPropertyListing';
 import Portfolio from '@homzhub/web/src/screens/portfolio';
+import SearchProperty  from '@homzhub/web/src/screens/searchProperty'
 
 const Financials = lazy(() => import('@homzhub/web/src/screens/financials'));
 const PostProperty = lazy(() => import('@homzhub/web/src/screens/addProperty/index'));
 const HelpAndSupport = lazy(() => import('@homzhub/web/src/screens/helpAndSupport'));
 
 export const MainRouter = (): React.ReactElement => {
-  const { DASHBOARD, ADD_PROPERTY, FINANCIALS, HELP_SUPPORT, ADD_LISTING, PORTFOLIO } = RouteNames.protectedRoutes;
+  const { DASHBOARD, ADD_PROPERTY, FINANCIALS, HELP_SUPPORT, ADD_LISTING, PORTFOLIO, SEARCH_PROPERTY } = RouteNames.protectedRoutes;
   const { APP_BASE } = RouteNames.publicRoutes;
   const { t } = useTranslation();
 
@@ -24,6 +25,7 @@ export const MainRouter = (): React.ReactElement => {
         <Route exact path={HELP_SUPPORT} component={HelpAndSupport} />
         <Route exact path={ADD_LISTING} component={AddPropertyListing} />
         <Route exact path={PORTFOLIO} component={Portfolio} />
+        <Route exact path ={SEARCH_PROPERTY} component={SearchProperty}/>
         <Redirect exact path={APP_BASE} to={DASHBOARD} />
       </Switch>
     </Suspense>
