c56ac83ccb3a564fbe08464e39a8b799c5232b15
ashwin712 <ashwin.karthik@nineleaps.com>
2021-03-03 20:54:53 +0530

    Route Search Property

---
+++ packages/web/src/screens/searchProperty/components/PropertyCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native'
+
+ const PropertyCard = () =>{
+    return (
+        <View>
+    Property Card       
+ </View>
+    )
+}
+
+export default PropertyCard;
