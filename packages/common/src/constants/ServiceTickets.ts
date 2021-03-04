551a6db6df41364392d927113121afd9be6584c7
K V N Sai Praharsh Bhagavan <53255161+praharshkvs1998@users.noreply.github.com>
2021-03-04 15:36:13 +0530

    HOM-60 : Add ServiceTicketDetails screen with mock data

--- packages/common/src/constants/ServiceTickets.ts
+++ packages/common/src/constants/ServiceTickets.ts
@@ -19,3 +19,46 @@ export const initialQuotes = [
     document: '',
   },
 ];
+
+export const sampleDetails = [
+  {
+    type: `${translationKey}:createdOn`,
+    value: '22 Sep, 2020 - 20:20',
+  },
+  {
+    type: `${translationKey}:updatedOn`,
+    value: '23 Sep, 2020 - 20:20',
+  },
+  {
+    type: `${translationKey}:ticketRaised`,
+    value: 'Ticket Raised',
+  },
+  {
+    type: `${translationKey}:assignedTo`,
+    value: 'Homzhub',
+  },
+  {
+    type: `${translationKey}:timeElapsed`,
+    value: '7 Days',
+  },
+  {
+    type: `${translationKey}:ticketNo`,
+    value: '#1234567890',
+  },
+];
+
+export const mockTicket = {
+  date: '23, Nov 2020',
+  title: 'Sample Ticket Title',
+  activity: {
+    stage1: {
+      role: 'Owner',
+      avatar:
+        'https://hmzhbdev.s3.ap-south-1.amazonaws.com/asset_images/c1a1aac4-5108-11eb-8e84-0242ac110004IMG_0002.JPG',
+      status: `${translationKey}:ticketRaised`,
+      description: `${translationKey}:ticketRaisedDescription`,
+      time: '11:00 AM',
+      timeLine: [`${translationKey}:ticketRaisedToOwner`, `${translationKey}:awaitingOwnerAction`],
+    },
+  },
+};
