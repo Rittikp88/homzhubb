230016d048bd1b96c150c883639bd52ae752e12b
Shikha Rai <48086446+raiShikha@users.noreply.github.com>
2021-11-24 14:00:23 +0530

    FFM-8 : Mixpanel setup

--- packages/ffm/src/App.tsx
+++ packages/ffm/src/App.tsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@homzhub/common/src/modules/store';
 import { I18nService } from '@homzhub/common/src/services/Localization/i18nextService';
+import { AnalyticsService } from '@homzhub/common/src/services/Analytics/AnalyticsService';
 import { StoreProviderService } from '@homzhub/common/src/services/StoreProviderService';
 import { StorageKeys, StorageService } from '@homzhub/common/src/services/storage/StorageService';
 import { RootNavigator } from '@homzhub/ffm/src/navigation/RootNavigator';
-import { SupportedLanguages } from '@homzhub/common/src/services/Localization/constants';
 import ErrorBoundary from '@homzhub/mobile/src/components/HOC/ErrorBoundary';
+import { SupportedLanguages } from '@homzhub/common/src/services/Localization/constants';
 
 StoreProviderService.init(configureStore);
 const store = StoreProviderService.getStore();
@@ -20,6 +21,7 @@ const App: () => React.ReactElement = () => {
   const bootUp = async (): Promise<void> => {
     const selectedLanguage: SupportedLanguages | null = await StorageService.get(StorageKeys.USER_SELECTED_LANGUAGE);
     await I18nService.init(selectedLanguage || undefined);
+    await AnalyticsService.initMixpanel();
 
     setBooting(false);
   };
